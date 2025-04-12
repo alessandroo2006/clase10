9fa4ef078c006bbabcd82b38ce62d6d8c4c425bf
Robertson Muñoz <rmunozp1@miumg.edu.gt>
2025-04-11 22:29:06 -0600

    formulario y crud

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1.clases;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1: Form
+    {
+
+        Crud miCrud = new Crud();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+
+
+
+
+
+        }
+
+        private void hola(object sender, EventArgs e)
+        {
+            MessageBox.Show("HOla te saludo desde el formulario, saludos compañeros ");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string nombre = textBoxEstudiante.Text;
+            string carnet = textBoxCarnet.Text;
+            string email = textBoxEmail.Text;
+            string seccion = textBoxSeccion.Text;
+            string respuesta = miCrud.AgregarAlumno(carnet, nombre, email, seccion);
+            MessageBox.Show(respuesta);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            textBoxEstudiante.Text = miCrud.MostrarInformacion(textBoxCarnet.Text);
+            textBoxNotas.Text = miCrud.MostrarTareas(textBoxCarnet.Text);
+            textBoxSeccion.Text = miCrud.MostrarInformacionSeccion(textBoxCarnet.Text);
+            textBoxEmail.Text = miCrud.MostrarInformacionCorreo(textBoxCarnet.Text);
+        }
+    }
+}
---
+++ WindowsFormsApp1/clases/Crud.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.clases
+{
+    public class Crud
+    {
+        string connectionString = "Server=ASUSMUÑOZ\\SQLEXPRESS;Database=UMG;Integrated Security=True; TrustServerCertificate=True; ";
+        public string MostrarInformacion(string carnet)
+        {
+            string nombre = "No Existe";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                String query = $"SELECT * FROM Tb_alumnos where carnet = '{carnet}' ";
+                try
+                {
+                    SqlCommand command = new SqlCommand(query, connection);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        nombre = reader["estudiante"].ToString();
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    nombre = "Error";
+                    //Console.WriteLine("Revisa y averigua el error, Error al conectar a la base de datos: " + ex.Message);
+                }
+                connection.Close();
+            }
+            return nombre;
+        }
+
+
+        public string MostrarInformacionSeccion(string carnet)
+        {
+
+            string seccion = "No Existe";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                String query = $"SELECT * FROM Tb_alumnos where carnet = '{carnet}'";
+                try
+                {
+                    SqlCommand command = new SqlCommand(query, connection);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+
+                        seccion = reader["seccion"].ToString();
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                    seccion = "Error";
+
+
+                    //Console.WriteLine("Revisa y averigua el error, Error al conectar a la base de datos: " + ex.Message);
+                }
+                connection.Close();
+            }
+            return seccion;
+
+        }
+        public string MostrarInformacionCorreo(string carnet)
+        {
+
+            string correo = "No Existe";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                String query = $"SELECT * FROM Tb_alumnos where carnet = '{carnet}'";
+                try
+                {
+                    SqlCommand command = new SqlCommand(query, connection);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+
+                        correo = reader["email"].ToString();
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                    correo = "Error";
+
+
+                    //Console.WriteLine("Revisa y averigua el error, Error al conectar a la base de datos: " + ex.Message);
+                }
+                connection.Close();
+            }
+            return correo;
+
+        }
+
+
+        public string AgregarAlumno(string carnet, string nombre, string email, string seccion)
+        {
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                String query = "insert into tb_alumnos (carnet, estudiante, email, seccion) values(@carnet,@nombre,@email,@seccion)";
+                try
+                {
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@carnet", carnet);
+                    command.Parameters.AddWithValue("@nombre", nombre);
+                    command.Parameters.AddWithValue("@email", email);
+                    command.Parameters.AddWithValue("@seccion", seccion);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    return "Registros insertados exitosamente 🎠🎈🎈🎈";
+
+                }
+                catch (Exception ex)
+                {
+                    return "Revisa y averigua el error, Error al conectar a la base de datos: " + ex.Message;
+                }
+                connection.Close();
+            }
+        }
+
+
+        public string MostrarTareas(string carnet)
+        {
+            string resultado = "No Existe";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = $"SELECT nota1, nota2, nota3, nota4 FROM tareas WHERE carnet = @carnet";
+                try
+                {
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@carnet", carnet);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        resultado = $"Nota1: {reader["nota1"]}, Nota2: {reader["nota2"]}, Nota3: {reader["nota3"]}, Nota4: {reader["nota4"]}";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    resultado = "Error";
+                    //Console.WriteLine("Revisa y averigua el error, Error al conectar a la base de datos: " + ex.Message);
+                }
+                connection.Close();
+            }
+            return resultado;
+        }
+
+
+    }
+}
